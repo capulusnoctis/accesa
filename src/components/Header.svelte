9e7c58f0a511ab457d9fdaabb284ad41a413cce3
capulusnoctis <axel.solares.g@gmail.com>
2020-09-06 23:54:43 -0600

    Added title to header.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,11 +8,18 @@ import Menubar from "./Menubar.svelte";
     div.Header {
         grid-column: 1 / 5;
     }
+    div.Header-title {
+        font-size: 28px;
+        color: #49484D;
+    }
+    .highlight-blue {
+        color: #0077B6;
+    }
 </style>
 
 <div class="Header">
     <Menubar/>
     <div class="Header-title">
-        
+        <h1>Accesa <span class="highlight-blue">Reciclaje</span></h1>
     </div>
 </div>
